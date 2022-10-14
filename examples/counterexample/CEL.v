d6ff78d8432e62b5584c42578f8629ea5c8d47fc
alxest <youngju.song@sf.snu.ac.kr>
2022-10-14 12:08:07 +0200

    Fix to compile

--- examples/counterexample/CEL.v
+++ examples/counterexample/CEL.v
@@ -10,21 +10,3 @@ Set Implicit Arguments.
 
 
 
-Section PROOF.
-
-  Definition fF: list val -> itree Es val :=
-    fun varg => b <- trigger (Take bool);; Ret b.
-
-  Definition FSem: ModSem.t := {|
-    ModSem.fnsems := [("f", cfunU fF)];
-    ModSem.mn := "F";
-    ModSem.initial_st := tt↑;
-  |}
-  .
-
-  Definition F: Mod.t := {|
-    Mod.get_modsem := fun _ => FSem;
-    Mod.sk := [("f", Sk.Gfun)];
-  |}
-  .
-End PROOF.
